9505e8e7feeb23dd8a81a879c6760663476469f4
auswitch <auswitch07@gmail.com>
2023-07-24 13:56:34 +0700

    create frame

--- src/pages/instructorDashboard.jsx
+++ src/pages/instructorDashboard.jsx
@@ -5,11 +5,14 @@ import sidebaricon from '../icon/sidebaricon.svg';
 
 export default function Dashboard() {
   return (
+    <>
     <div className='navbar'>
       <img src={sidebaricon} alt="SidebarIcon" className="sidebar-icon" />
       <img src={cmulogo} alt="CMULogo" className="cmulogo" />
-      <p className="hellostyle" >Hello, Dome P.</p>
+      <p className="hellostyle">Hello, Dome P.</p>
       <p className="rolestyle">1/66, Instructor</p>
     </div>
+    <div className='framewindow'></div>
+    </>
   );
 }
