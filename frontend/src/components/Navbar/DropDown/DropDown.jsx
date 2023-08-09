f6060c131d1d4dc3874ec46f74c6adfb8eb0baa2
worapit <drm.wmwork@gmail.com>
2023-08-09 23:47:19 +0700

    make select sec

--- frontend/src/components/Navbar/DropDown/DropDown.jsx
+++ frontend/src/components/Navbar/DropDown/DropDown.jsx
@@ -1,13 +1,69 @@
-import React from "react";
-import styleDrop from './dropdown.module.css';
+import React, { useState } from "react";
+import styleDrop from "./dropdown.module.css";
 
 const DropDown = () => {
-    return(
-        <div>
-            <p className={ styleDrop.box-drop}>Select section</p>
-        </div>
-    );
+  const [isDropDown, setDropDown] = useState(false);
+  const [isSelectSec, setSelectSec] = useState("Select Section");
+
+  const onClickDropDown = () => {
+    setDropDown(!isDropDown);
+  };
+  const onClickSection = (sec) => {
+    setSelectSec(sec);
+    setDropDown(false);
+  };
+
+  const styleArrow = {
+    transform: isDropDown ? "rotate(180deg)" : "rotate(0deg)",
+    transition: "tranform 0.3s ease",
+  };
+
+  return (
+    <div className={styleDrop.boxdrop}>
+      <div>
+        <p className={`${styleDrop.box_select} ${styleDrop.font}`}>
+          {isSelectSec ? isSelectSec : "Select section"}
+          <svg
+            onClick={onClickDropDown}
+            style={styleArrow}
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="14"
+            viewBox="0 0 26 16"
+            fill="none"
+          >
+            <path
+              d="M1.96317 2.00391L13.4632 13.0039L24.9632 2.00391"
+              stroke="black"
+              strokeWidth="3"
+            />
+          </svg>
+        </p>
+      </div>
+
+      {isDropDown && (
+        <ul className={styleDrop.sec_menu}>
+          <li
+            className={`${styleDrop.option} ${styleDrop.font}`}
+            onClick={() => onClickSection("Section 001")}
+          >
+            Section 001
+          </li>
+          <li
+            className={`${styleDrop.option} ${styleDrop.font}`}
+            onClick={() => onClickSection("Section 002")}
+          >
+            Section 002
+          </li>
+          <li
+            className={`${styleDrop.option} ${styleDrop.font}`}
+            onClick={() => onClickSection("Section 801")}
+          >
+            Section 801
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
 export default DropDown;
-
-
