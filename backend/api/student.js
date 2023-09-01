67122e9095d8882fa75c6f6198da67793be1d9b2
Coolestwaii <yougle55567@gmail.com>
2023-09-01 20:28:42 +0700

    add get score of student

--- backend/api/student.js
+++ backend/api/student.js
@@ -6,26 +6,39 @@ const router = express.Router();
 
 // add student grade
 router.post("/add", async (req, res) => {
-    try {
-        const token = req.cookies.token;
-        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-            if (err)
-                return res.status(401).send({ ok: false, message: "Invalid token"});
-            else if (!user.cmuAccount)
-                return res.status(403).send({ ok: false, message: "Invalid token"});
-        })
-        const decoded = jwt.decode(token);
+  try {
+    const token = req.cookies.token;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err)
+        return res.status(401).send({ ok: false, message: "Invalid token" });
+      else if (!user.cmuAccount)
+        return res.status(403).send({ ok: false, message: "Invalid token" });
+    });
+    const decoded = jwt.decode(token);
 
-        console.log(req.body)
+    console.log(req.body);
 
-        // const newStudentGrade = await studentModel.create({
-        //     studentID: req.body
-        // })
-        newStudentGrade.save();
-        return res.send(newStudentGrade);
-    } catch (err) {
-        return err;
-    }
-})
+    // const newStudentGrade = await studentModel.create({
+    //     studentID: req.body
+    // })
+    newStudentGrade.save();
+    return res.send(newStudentGrade);
+  } catch (err) {
+    return err;
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const scores = await studentModel.findOne({
+      studentId: req.query.studentId,
+    });
+    if (!scores)
+      return res.send({ ok: true, message: "You don't have any score" });
+    return res.send(scores);
+  } catch (err) {
+    return err;
+  }
+});
 
 module.exports = router;
