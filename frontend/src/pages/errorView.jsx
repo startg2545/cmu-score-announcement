000d4c7b535c58fe51a8e05dacbc0a57dcbdf1a5
auswitch <90751246+auswitch@users.noreply.github.com>
2023-09-05 22:35:26 +0700

    edit navbar

--- frontend/src/pages/errorView.jsx
+++ frontend/src/pages/errorView.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from "react";
-import './signIn.css';
+import React, { useContext, useEffect, useState } from "react";
+import "./signIn.css";
+import { ROLE, UserInfoContext } from "../context";
 
 const ErrorView = () => {
+  const pathname = window.location.pathname;
+  const {userInfo} = useContext(UserInfoContext)
+  const getCurrentDimension = () => {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  };
 
-    const getCurrentDimension = () => {
-        return {
-            width: window.innerWidth,
-            height: window.innerHeight
-        }
+  const [screenSize, setScreenSize] = useState(getCurrentDimension());
+  useEffect(() => {
+    const updateDimension = () => {
+      setScreenSize(getCurrentDimension());
+    };
+    window.addEventListener("resize", updateDimension);
+
+    return () => {
+      window.removeEventListener("resize", updateDimension);
+    };
+  }, [screenSize]);
+
+  useEffect(() => {
+    if (
+      userInfo.itAccountType === ROLE.INSTRUCTOR &&
+      pathname === "/errorView" &&
+      (screenSize.width >= 1200 && screenSize.height >= 500)
+    ) {
+      window.location.replace("/sign-in");
     }
-
-    const [screenSize, setScreenSize] = useState(getCurrentDimension());
-    useEffect(() => {
-        const updateDimension = () => {
-            setScreenSize(getCurrentDimension())
-          }
-          window.addEventListener('resize', updateDimension);
-          
-          
-      
-          return(() => {
-              window.removeEventListener('resize', updateDimension);
-          })
-    }, [] )
-
-    useEffect (() => {
-        console.log(screenSize);
-        if(screenSize.width >= 900 || screenSize.height >= 1200 || screenSize.width >= 1200 || screenSize.height >= 900)
-          {
-            window.location.replace("/sign-in");
-          }
-    },[screenSize]);
+  }, [screenSize]);
 
   return (
-
     <div>
-        <div className="popuplogin">
-        
+      <div className="popuplogin">
         <div className="topictext">SCORE ANNOUNCEMENT </div>
         <div className="logintext">ลงชื่อเข้าสู่ระบบ</div>
       </div>
