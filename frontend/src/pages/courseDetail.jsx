c66885ecf33432389aebb0f3728209c9e1934799
startg2545 <74952330+startg2545@users.noreply.github.com>
2023-09-07 19:48:33 +0700

    retrieve sections information

--- frontend/src/pages/courseDetail.jsx
+++ frontend/src/pages/courseDetail.jsx
@@ -3,7 +3,9 @@ import { getScores } from '../services';
 import { useSearchParams } from 'react-router-dom';
 
 const CourseDetail = () => {
+  const [sections, setSections] = useState([])
   const [scores, setScores] = useState([])
+  const [selectedSection, setSelectedSection] = useState()
   const [searchParams, setSearchParams] = useSearchParams({});
   const [params, setParams] = useState({})
 
@@ -14,55 +16,77 @@ const CourseDetail = () => {
       courseNo: searchParams.get("courseNo"),
     })
   }, [searchParams])
+
   useEffect(()=>{
     const fetchData = async () => {
       const resp = await getScores();
+      console.log(resp)
       if (resp) {
         resp.map(data=>{
           if (data.courseNo === params.courseNo) {
-            let scores = data.sections
-            setScores(scores)
+            let sections = data.sections
+            setSections(sections)
+            console.log(sections)
           };
         })
       }
-      console.log('courseNo', params.courseNo)
-      console.log('year', params.year)
-      console.log('semaster', params.semester)
     };
     fetchData();
   }, [params])
+
+  const getAverage = (results) => {
+    let avg = 0
+    for (let x in results) {
+      avg += results[x].point
+    }
+    return Math.round( avg * 100 ) / 100
+  }
+
   return (
     <div>
-    {/* <table id='table'>
-      <tbody>
-      <tr>
-        <th>Number</th>
-        <th>Score Name</th>
-        <th>Number of Students</th>
-        <th>Full Score</th>
-        <th>Mean</th>
-        <th>Publish</th>
-        <th>Management</th>
-      </tr>
-      {scores.map((data,key)=>{
-        return (
-          <tr key={key}>
-            <td>{key+1}</td>
-            <td>{data.scoreName}</td>
-            <td>{data.studentNumber}</td>
-            <td>{data.fullScore}</td>
-            <td>{data.mean}</td>
-            <td><button>[publish]</button></td>
-            <td>
-              <button>add</button>
-              <button>edit</button>
-              <button>delete</button>
-            </td>
-          </tr>
-        )
+      <label><b>Course No</b>: {params.courseNo}</label>
+      {sections.map((data,index)=>{
+          return (
+            <div key={index}>
+              <label onClick={()=>{
+                setScores(data.scores) 
+                setSelectedSection(index)
+                }}
+              ><button><b>Section</b>: {data.section}</button></label>
+              { index == selectedSection ? 
+              <table>
+                <tbody>
+                  <tr>
+                    <th>Number</th>
+                    <th>Score Name</th>
+                    <th>Student Number</th>
+                    <th>Full Score</th>
+                    <th>Average Score</th>
+                    <th>Publish</th>
+                    <th>Management</th>
+                  </tr>
+                  {scores.map((data,index)=>{
+                    return (
+                      <tr key={index}>
+                        <td>{index+1}</td>
+                        <td>{data.scoreName}</td>
+                        <td>{data.studentNumber}</td>
+                        <td>{data.fullScore}</td>
+                        <td>{getAverage(data.results)}</td>
+                        <td><button>[open]</button></td>
+                        <td>
+                          <button>[add]</button>
+                          <button>[edit]</button>
+                          <button>[delete]</button>
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table> : null }
+            </div>
+          )
       })}
-      </tbody>
-    </table> */}
     </div>
   )
 }
