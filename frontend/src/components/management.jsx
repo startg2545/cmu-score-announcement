1eec196d9439aadd7f920f4569e40f042c0b2956
worapit <drm.wmwork@gmail.com>
2023-09-09 00:44:29 +0700

    section management view

--- frontend/src/components/management.jsx
+++ frontend/src/components/management.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
 import secMan from "./css/manage.module.css";
 
-const Management = () => {
+const Management = (data) => {
   return (
-    <div className={secMan.layOut}>
-      <h1 className={secMan.secBox}>Section 001</h1>
-     
-
+    <div >
+      <div className={secMan.layOut}>
+        <h1 className={secMan.secBox}>Section 001</h1>
+        <h1 className={secMan.secBox}>Section 002</h1>
+        <h1 className={secMan.secBox}>Section 003</h1>
+        <h1 className={secMan.secBox}>Section 004</h1>
+        <h1 className={secMan.secBox}>Section 005</h1>
+        <h1 className={secMan.secBox}>Section 006</h1>
+      </div>
     </div>
   );
 };
