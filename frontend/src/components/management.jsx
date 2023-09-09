1a3398eec6aae34fa974ddbc126f60a22799a788
auswitch <90751246+auswitch@users.noreply.github.com>
2023-09-09 14:52:29 +0700

    edit management pg.

--- frontend/src/components/management.jsx
+++ frontend/src/components/management.jsx
@@ -1,39 +1,83 @@
 import React, { useState, useEffect, useContext } from "react";
 import secMan from "./css/manage.module.css";
-
+import { ShowSidebarContext, UserInfoContext } from "../context";
 import TableScore from "./TableScore";
 
-const Management = ({ data , dataTable}) => {
- const [isShowTable , setIsShowTable] = useState(false);
-//  const [isSelectSec , setIsSelectSec] = useState(false);
+const Management = ({ data, dataTable }) => {
+  const [isShowTable, setIsShowTable] = useState(false);
+  //  const [isSelectSec , setIsSelectSec] = useState(false);
 
- const showTable = () => {
+  const showTable = () => {
     setIsShowTable(true);
- };
- 
+  };
+
+  const { showSidebar } = useContext(ShowSidebarContext);
+
   return (
     <div>
-        {!isShowTable&&
-           <div className={secMan.layOut}>
-           {data.map((e, key) => (
-               <p className={secMan.secBox} key={key}
-               onClick={() => {
-                 showTable()
-               }}>
-                 Section
-                 {e.section < 10
-                   ? `00${e.section}`
-                   : e.section < 100
-                   ? `0${e.section}`
-                   : e.section}
-               </p>
-           ))}
-           </div>
-        }
-      
-      {isShowTable &&  <TableScore data={dataTable} />}
+      <div
+        className={` ${secMan.Managecourseframewindow}  ${
+          showSidebar ? secMan.shrink : ""
+        }`}
+      >
+        {!isShowTable && (
+          <div >
+            {data.map((e, key) => (
+              <p
+                className={secMan.secBox}
+                key={key}
+                onClick={() => {
+                  showTable();
+                }}
+              >
+                Section
+                {e.section < 100
+                  ? `00${e.section}`
+                  : e.section < 100
+                  ? `0${e.section}`
+                  : e.section}
+              </p>
+            ))}
+            {data.map((e, key) => (
+              <p
+                className={secMan.secBox}
+                key={key}
+                onClick={() => {
+                  showTable();
+                }}
+              >
+                Section
+                {e.section < 100
+                  ? `00${e.section}`
+                  : e.section < 100
+                  ? `0${e.section}`
+                  : e.section}
+              </p>
+            ))}
+            {data.map((e, key) => (
+              <p
+                className={secMan.secBox}
+                key={key}
+                onClick={() => {
+                  showTable();
+                }}
+              >
+                Section
+                {e.section < 100
+                  ? `00${e.section}`
+                  : e.section < 100
+                  ? `0${e.section}`
+                  : e.section}
+              </p>
+            ))}
+
+            
+          </div>
+        )}
+      </div>
+
+      {isShowTable && <TableScore data={dataTable} />}
     </div>
-   
   );
 };
 
