a413a172c2dd953fd5e09bcfb6c5d6f397d5a13f
mon6996 <thanaporn_chan@cmu.ac.th>
2023-09-12 22:27:23 +0700

    edit add co-instructor

--- backend/api/course.js
+++ backend/api/course.js
@@ -105,6 +105,8 @@ router.put("/", async (req, res) => {
       { new: true }
     );
 
+    console.log(result.sections);
+
     await result.save();
     return res.send("Co-Instructor have been added.");
   } catch (err) {
