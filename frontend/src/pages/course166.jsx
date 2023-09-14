cdb7bf97fcb271134de08e213078147951797d05
mon6996 <thanaporn_chan@cmu.ac.th>
2023-09-15 02:32:56 +0700

    update index, import, page

--- frontend/src/pages/course166.jsx
+++ frontend/src/pages/course166.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useContext, useMemo } from "react";
 import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
 import Course from "./css/course166.module.css";
-import { SideBar, TableScore, UploadSc } from "../components";
+import { SideBar, UploadSc } from "../components";
 import { ShowSidebarContext } from "../context";
 import { addCourse, getAllCourses, getScores } from "../services";
-import DropDownCourse from "../components/DropDownCourse";
 import { TextInput, Button, Flex, Modal } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { SideBar } from "../components";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to CMU Score Annnouncement</h1>
-        </div>
-    );
+  return (
+    <div>
+      <SideBar />
+      <h1>Welcome to CMU Score Annnouncement</h1>
+    </div>
+  );
 };
 
 export default Home;
