7fa366b3a33cd756f5577e5f08b76c161610bad5
Coolestwaii <yougle55567@gmail.com>
2023-09-18 02:07:41 +0700

    fixed instructor mobile sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -12,5 +11,4 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 };
