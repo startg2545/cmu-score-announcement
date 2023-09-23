5240f824d5137bad482c98677d2e5f9a1e29af1b
startg2545 <newin_yamaguchi@cmu.ac.th>
2023-09-23 16:36:17 +0700

    create current context

--- frontend/src/context/index.jsx
+++ frontend/src/context/index.jsx
@@ -1,5 +1,6 @@
 import { PAGE, ROLE } from "./enum";
 import ShowSidebarContext from "./showSidebarContex";
 import UserInfoContext from "./userInfo";
+import CurrentContext from "./current";
 
-export { PAGE, ROLE, ShowSidebarContext, UserInfoContext };
+export { PAGE, ROLE, ShowSidebarContext, UserInfoContext, CurrentContext };
