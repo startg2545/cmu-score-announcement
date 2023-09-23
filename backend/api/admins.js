dab7d805b0272063a5ccba96e6957555ac0d4a1b
auswitch <90751246+auswitch@users.noreply.github.com>
2023-09-23 21:46:53 +0700

    add socket when admin updateCurrent

--- backend/api/admins.js
+++ backend/api/admins.js
@@ -1,70 +1,74 @@
 const express = require("express");
 const { verifyAndValidateToken } = require("../jwtUtils");
 const adminModel = require("../db/adminSchema");
-router = express.Router();
+const router = express.Router();
 
 router.post("/delete", async (req, res) => {
-    try {
+  try {
+    const socket = req.app.get("socket");
+    const _id = req.body._id;
 
-        
-        const _id = req.body._id
+    const current = await adminModel.findOneAndDelete({ _id: _id });
 
-        const current = await adminModel.findOneAndDelete({ _id: _id });
-
-        return res.send('This is item has been deleted.')
-
-    } catch (err) {
-        console.log(err)
-        return res
-            .status(500)
-            .send({ ok: false, message: "Internal Server Error" })
-    }
-})
+    socket.emit("currentUpdate", current);
+    return res.send("This is item has been deleted.");
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal Server Error" });
+  }
+});
 
 router.post("/", async (req, res) => {
-    try {
-        const token = req.cookies.token;
-        const user = await verifyAndValidateToken(token);
-        if (!user.cmuAccount) return res.status(403).send({ ok: false, message: "Invalid token" });
-        
-        const semester = parseInt(req.body.semester)
-        const year = parseInt(req.body.year)
+  try {
+    const token = req.cookies.token;
+    const user = await verifyAndValidateToken(token);
+    if (!user.cmuAccount)
+      return res.status(403).send({ ok: false, message: "Invalid token" });
 
-        const current = await adminModel.findOne({ semester, year });
+    const socket = req.app.get("socket");
+    const semester = parseInt(req.body.semester);
+    const year = parseInt(req.body.year);
 
-        if (current) {
-            return res.send("This current already exists, pleaes try again.")
-        } else {
-            const new_current = await adminModel.create({
-                admin: user.cmuAccount,
-                semester,
-                year
-            })
-            return res.send('New current has been added.')
-        }
-    } catch (err) {
-        console.log(err)
-        return res
-            .status(500)
-            .send({ ok: false, message: "Internal Server Error" })
+    const current = await adminModel.findOne({ semester, year });
+
+    if (current) {
+      socket.emit("currentUpdate", current);
+      return res.send("This current already exists, pleaes try again.");
+    } else {
+      const new_current = await adminModel.create({
+        admin: user.cmuAccount,
+        semester,
+        year,
+      });
+      socket.emit("currentUpdate", new_current)
+      return res.send("New current has been added.");
     }
-})
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal Server Error" });
+  }
+});
 
-router.get("/", async (req,res) =>{
-    try {
-        const token = req.cookies.token;
-        const user = await verifyAndValidateToken(token);
-        if (!user.cmuAccount) return res.status(403).send({ ok: false, message: "Invalid token" });
+router.get("/", async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    const user = await verifyAndValidateToken(token);
+    if (!user.cmuAccount)
+      return res.status(403).send({ ok: false, message: "Invalid token" });
 
-        const currents = await adminModel.find({})
+    const currents = await adminModel.find({});
 
-        return res.send(currents)
-    } catch (err) {
-        console.log(err)
-        return res
-            .status(500)
-            .send({ ok: false, message: "Internal Server Error" })
-    }
-})
+    return res.send(currents);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .send({ ok: false, message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
