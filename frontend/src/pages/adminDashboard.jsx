e9a9998a99a3b755509a99805742597050458ea8
worapit <drm.wmwork@gmail.com>
2023-09-30 14:32:51 +0700

    resolve style adminDashboard

--- frontend/src/pages/adminDashboard.jsx
+++ frontend/src/pages/adminDashboard.jsx
@@ -29,9 +29,10 @@ const AdminDashboard = () => {
         const enteredYear = parseInt(value);
         if (isNaN(enteredYear)) {
           return "Invalid year for B.E. program";
-        } else if (enteredYear > currentYear || enteredYear < currentYear - 1) {
-          return "Year must be the current year or less than 1 year.";
         }
+        // else if (enteredYear > currentYear || enteredYear < currentYear - 1) {
+        //   return "Year must be the current year or less than 1 year.";
+        // }
       },
     },
     validateInputOnBlur: true,
@@ -78,8 +79,8 @@ const AdminDashboard = () => {
 
   const showCurrent = current?.map((element, key) => {
     return (
-      <label className="py-4" key={key}>
-        <div className="py-1 text-xl">
+      <label className="py-1" key={key}>
+        <div className="bg-white drop-shadow border-black-50 border-[1px] text-lg w-[300px] h-[50px] rounded-xl flex justify-between items-center px-10">
           {element.semester}/{element.year}
           <button
             style={{ color: "red" }}
@@ -97,10 +98,10 @@ const AdminDashboard = () => {
     <>
       <div className="flex flex-row gap-3 justify-center ">
         <div className="flex w-full flex-col h-full">
-          <div className="mx-[1%] lg:mt-3 max-h-screen ">
-            <div className="lg:rounded-xl rounded-xl xl:h-[calc(84vh-60px)] lg:h-[calc(83vh-60px)] md:h-[calc(85vh-55px)]  h-[calc(85vh-50px)] overflow-hidden border-[3px] border-primary mt-24 ">
+          <div className="mx-[2%] lg:mt-3 max-h-screen ">
+            <div className="lg:rounded-xl rounded-xl xl:h-[calc(89vh-60px)] lg:h-[calc(88vh-60px)] md:h-[calc(89vh-55px)]  h-[calc(88vh-50px)] overflow-hidden border-[3px] border-primary mt-24 ">
               <div className="flex flex-col ">
-                <div className="bg-primary lg:py-2 py-2 lg:px-5 px-3 flex flex-row w-full items-center justify-between cursor-default mb-3">
+                <div className="bg-primary lg:py-2 py-2 lg:px-5 px-3 flex flex-row w-full items-center justify-between cursor-default ">
                   <div className="flex items-start flex-col justify-center ">
                     <p className="text-white font-semibold xl:text-4xl lg:text-4xl md:text-3xl text-3xl">
                       {
@@ -126,35 +127,48 @@ const AdminDashboard = () => {
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col  px-5 xl:h-[calc(84vh-175px)] lg:h-[calc(83vh-197px)] md:h-[calc(85vh-207px)] h-[calc(85vh-193px)]">
-                  <p
-                    className="xl:text-3xl lg:text-2xl md:text-xl font-semibold  text-xl
-                                  ˝         xl:block lg:block md:block  text-primary mb-1"
-                  >
-                    Semester currently in active
-                  </p>
-                  <ScrollArea w={300} h="fit-content" type="always">
-                    <div className="px-4 text-md flex-col">
-                      {showCurrent}
+                <div
+                  className="flex flex-row  px-10 py-5 justify-between 
+                                xl:h-[calc(89vh-148px)] lg:h-[calc(88vh-148px)] md:h-[calc(89vh-140px)] h-[calc(88vh-130px)]"
+                >
+                  <div className="bg-white drop-shadow-xl border-black-70 border-[1px] rounded-xl w-[500px] 
+                                    flex justify-center py-5 ">
+                    <div>
+                      <p className="xl:text-3xl lg:text-2xl md:text-xl font-semibold text-xl xl:block lg:block md:block text-primary mb-1">
+                        Semester currently in active
+                      </p>
+                      {/* <ScrollArea type="always" >
+                        <div className="border-[2px] border-black-50 py-5 
+                                        mt-5 justify-center text-md flex-col">
+                          {showCurrent}
+                        </div>
+                      </ScrollArea> */}
+                      <div className="py-2 h-5/6 overflow-y-auto  
+                                      mt-5 rounded-lg flex flex-col items-center text-md">
+                            {showCurrent}
+                      </div>
                     </div>
-                  </ScrollArea>
+                  </div>
                   <form
                     onSubmit={submitForm.onSubmit((data) => {
                       handleSubmit(data);
                     })}
+                    className="bg-white drop-shadow-lg border-black-70 border-[1px] rounded-xl
+                               flex flex-col items-center py-5 gap-5
+                               w-[850px]  h-[200px]"
                   >
                     <p
                       className="xl:text-3xl lg:text-2xl md:text-xl font-semibold  text-xl
-                                  ˝         xl:block lg:block md:block  text-primary py-4"
+                                 xl:block lg:block md:block  text-primary"
                     >
                       Add new semester and year
                     </p>
-                    <div className="px-4">
+                    <div className="flex flex-row justify-center items-center gap-10 ">
                       <Radio.Group
                         label="Semester"
                         withAsterisk
                         size="md"
-                        className=" mb-3"
+                        // className=" mb-3"
                         {...submitForm.getInputProps("semester")}
                       >
                         <Group mt="xs">
@@ -163,20 +177,7 @@ const AdminDashboard = () => {
                           <Radio value="3" label="3(Summer)" />
                         </Group>
                       </Radio.Group>
-                      {/* <TextInput
-                        type="text"
-                        placeholder="Add Semester fill only 1, 2 and 3"
-                        onChange={(e) => {
-                          const input = e.target.value;
-                          const sanitizedInput = input.replace(/[^1-3]/g, "");
-                          setSemester(sanitizedInput);
-                        }}
-                        value={semester}
-                        maxLength={1}
-                        className="mb-5"
-                      /> */}
-                    </div>
-                    <div className="px-4">
+
                       <TextInput
                         size="md"
                         label="Academic Year"
@@ -184,13 +185,11 @@ const AdminDashboard = () => {
                         type="text"
                         withAsterisk={true}
                         placeholder="Add Year"
-                        className="mb-2"
                       />
-                    </div>
-                    <div className="px-4">
+
                       <Button
+                        className=" bg-green-600 w-[100px] hover:bg-white hover:text-green-600 hover:border-green-600"
                         type="submit"
-                        style={{ backgroundColor: "Green", marginTop: "10px" }}
                       >
                         Confirm
                       </Button>
