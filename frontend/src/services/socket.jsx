84df6172e6c98b85426093b18c21dffaa3192567
mon6996 <thanaporn_chan@cmu.ac.th>
2023-10-09 22:40:54 +0700

    new build

--- frontend/src/services/socket.jsx
+++ frontend/src/services/socket.jsx
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
-export const socket = io({
-  path: process.env.REACT_APP_API_BASE_URL,
-  transports: ["websocket"],
+export const socket = io(process.env.REACT_APP_API_BASE_URL, {
+  transports: ["websocket", "polling"],
 });
